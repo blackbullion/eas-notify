7c2ed504ae1eea1540df856d034fd1d433b82bcc
Tudor <tudor@blackbullion.com>
2021-12-17 12:09:22 +0000

    handle skipped builds and only one build at a time

--- index.js
+++ index.js
@@ -20,40 +20,58 @@ const blocksConfig = {
 		},
 		{
 			type: 'divider'
-		},
-		{
-			type: 'image',
-			image_url: 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=$ANDROID_LINK',
-			alt_text: 'Android app build',
-			title: {
-				type: 'plain_text',
-				text: '🤖 Android'
-			}
-		},
-		{
-			type: 'divider'
-		},
-		{
-			type: 'image',
-			image_url: 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=$IOS_LINK',
-			alt_text: 'iOS app build',
-			title: {
-				type: 'plain_text',
-				text: '🍎 iOS'
-			}
 		}
 	]
+}
+
+const androidBuildBlock = {
+	type: 'image',
+	image_url: 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=$ANDROID_LINK',
+	alt_text: 'Android app build',
+	title: {
+		type: 'plain_text',
+		text: '🤖 Android'
+	}
+}
+
+const iosBuildBlock = {
+	type: 'image',
+	image_url: 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=$IOS_LINK',
+	alt_text: 'iOS app build',
+	title: {
+		type: 'plain_text',
+		text: '🍎 iOS'
+	}
 }
 
 const androidLinkSearch = '🤖 Android build details: '
 const iosLinkSearch = '🍎 iOS build details: '
 
+const androidCancelledSearch = '🤖 Android build was canceled'
+const iosCancelledSearch = '🍎 iOS build was canceled'
+
 const main = async () => {
   try {
     const content = (await fs.readFile(core.getInput('easOutputFile'), 'utf8')).split('\n')
 
-    const androidLink = content.find((line) => line.startsWith(androidLinkSearch)).split(androidLinkSearch)[1]
-    const iosLink = content.find((line) => line.startsWith(iosLinkSearch)).split(iosLinkSearch)[1]
+    const androidLink = content.find((line) => line.includes(androidLinkSearch)).split(androidLinkSearch)[1]
+    const iosLink = content.find((line) => line.includes(iosLinkSearch)).split(iosLinkSearch)[1]
+
+		const androidBuilt = androidLink && !content.find((line) => line.includes(androidCancelledSearch))
+		const iosBuilt = iosLink && !content.find((line) => line.includes(iosCancelledSearch))
+
+		if (!androidBuilt && !iosBuilt) {
+			core.info('No builds were completed. Skipping message.')
+			return
+		}
+
+		if (androidBuilt) {
+			blocksConfig.blocks = [...blocksConfig.blocks, androidBuildBlock]
+		}
+		if (iosBuilt) {
+			if (androidBuilt) blocksConfig.blocks = [...blocksConfig.blocks, { type: 'divider' }]
+			blocksConfig.blocks = [...blocksConfig.blocks, iosBuildBlock]
+		}
     
     const blocks = JSON.stringify(blocksConfig)
       .replace('$ANDROID_LINK', androidLink)
